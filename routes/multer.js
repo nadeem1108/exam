61c74b43f84afbc837a0ca2588d2f9939f9c7385
nadeem1108 <nadeemcoc8@gmail.com>
2021-07-24 16:55:06 +0530

    multer

--- routes/multer.js
+++ routes/multer.js
@@ -1,10 +1,12 @@
-
 const express = require("express");
 const app = express();
 const multer = require("multer");
 const path = require("path");
 var router = express.Router();
-
+const fs = require('fs')
+// const { promisify } = require('util')
+// const unlinkAsync = promisify(fs.unlink)
+// const dirPath = path.join(__dirname, '..', 'public/upload')
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -29,13 +31,30 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.post("/uploadfile", uploadMultiple, function (req, res, next) {
+router.post("/uploadfile", uploadMultiple, async (req, res, next) => {
+  
   if (req.files) {
-    console.log(req.files);
-
+    console.log(req.body.url);
+   console.log(req.files);
     console.log("files uploaded");
   }
 });
+
+module.exports=(req,res,next)=>{
+  const path = `../public/${req.body.url}`;
+  fs.unlink(path,(err)=>{
+  if (err){
+    res.status=false;
+  }else{
+    res.status=true;
+  }
+  next()
+  })
+}
+
+
+
+
 
 
 module.exports = router;
